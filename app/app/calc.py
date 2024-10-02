0ca55775449eac5d9ee7a8699d162f83feb67db3
Ana Stevanovic <paunovicana82@gmail.com>
2024-10-02 13:18:17 +0200

    psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""""
+
+Cakculated functions
+"""
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x, y ):
+    """Substract x and y and return resunlt."""
+    return y - x
