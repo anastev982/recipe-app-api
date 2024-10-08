a8a05380c54f7da2ec454fb295108249f4ed318c
Ana Stevanovic <paunovicana82@gmail.com>
2024-10-08 13:10:30 +0200

    Uppdating

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework.authtoken',
     "core",
 ]
 
@@ -129,3 +130,5 @@ STATIC_URL = "/static/"
 # docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'core.User'
